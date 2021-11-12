20424e30eb385cf9eb74c0469ed0e151489eb16e
Ravi Shukla <rshuklabbb@gmail.com>
2021-11-12 20:17:52 +0530

    Created Functional Mobile OS component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import UnOrderedList from './components/unordered list/UnOrderedList';
-import List from './components/List/List';
+// import UnOrderedList from './components/unordered list/UnOrderedList';
+// import List from './components/List/List';
+import MobileOS from './components/mobile os/MobileOS';
 
 function App() {
   return (
     <div className="App">
-      <UnOrderedList />
-      <List />
+      {/* <UnOrderedList />
+      <List /> */}
+      <MobileOS />
     </div>
   );
 }
---
+++ src/components/mobile os/MobileOS.jsx
@@ -0,0 +1,24 @@
+export default function MobileOS(){
+    return (
+        <>
+            <div>
+                <h3>Mobile Operating System</h3>
+                <ul>
+                    <li>Android</li>
+                    <li>Blackberry</li>
+                    <li>IOS</li>
+                    <li>Windows Phone</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Mobile Manufacturers</h3>
+                <ul>
+                    <li type="square" >Smasung</li>
+                    <li type="square" >HTC</li>
+                    <li>Micromax</li>
+                    <li type="" >Apple</li>
+                </ul>
+            </div>
+        </>
+    )
+}
